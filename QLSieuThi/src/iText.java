eaabfae02d73f6c50e2bb4d3f23247ffdb816481
minhminion <46214292+minhminion@users.noreply.github.com>
2019-05-16 19:30:08 +0700

    update

--- QLSieuThi/src/iText.java
+++ QLSieuThi/src/iText.java
@@ -2,7 +2,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
-import com.itextpdf.text.Anchor;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
